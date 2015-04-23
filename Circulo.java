a41a36eeafff78316d5148caddaaa6dd84fee597
VictorFurlan <victor@SOLO-TI-01.local>
2015-04-23 17:51:01 -0300

    Galera confere os métodos obrigatórios antes de repassar para as outras classes

--- Circulo.java
+++ Circulo.java
@@ -84,4 +84,101 @@ public class Circulo extends Figura
                ":" +
                this.getCor().getBlue();
     }
+    
+    // ****CONFERIR DAQUI PARA BAIXO****
+    
+    public int hashCode()
+    {
+    	int resultado =1;
+    	
+    	resultado = resultado*7 + this.raio;
+    	resultado = resultado*7 + this.centro.getX();
+    	resultado = resultado*7 + this.centro.getY();
+    	resultado = resultado*7 + this.cor.getRed();
+    	resultado = resultado*7 + this.cor.getGreen();
+    	resultado = resultado*7 + this.cor.getBlue();
+    	
+    	return resultado;
+    }
+    
+    public boolean equals (Object obj)
+    {
+        if (this==obj)
+            return true;
+
+        if (obj==null)
+            return false;
+        
+        if (obj instanceof Circulo)
+        {
+        	Circulo cir = (Circulo)obj;
+
+            if (cir.centro    == this.centro   &&
+            	cir.raio == this.raio          &&
+            	cir.cor == this.cor)
+                return true;
+        }
+        return false;
+    }
+    
+    public int compareTo (Circulo c)
+    {
+        if (this.centro.getX()<c.centro.getX())
+             return -7;
+
+        if (this.centro.getY()<c.centro.getY())
+            return -7;
+
+        if (this.raio<c.raio)
+            return -7;
+        
+        if (this.cor.getRed()<c.cor.getRed())
+            return -7;
+       
+       if (this.cor.getGreen()<c.cor.getGreen())
+           return -7;
+       
+       if (this.cor.getBlue()<c.cor.getBlue())
+           return -7;
+        
+        if (this.centro.getX()>c.centro.getX())
+            return 7;
+
+        if (this.centro.getY()>c.centro.getY())
+           return 7;
+        
+        if (this.raio>c.raio)
+            return 7;
+
+        if (this.cor.getRed()>c.cor.getRed())
+             return 7;
+        
+        if (this.cor.getGreen()>c.cor.getGreen())
+            return 7;
+        
+        if (this.cor.getBlue()>c.cor.getBlue())
+            return 7;
+        
+        return 0;
+    }
+    
+    public Object clone ()
+    {
+        Circulo c = null;
+
+        try
+        {
+            c = new Circulo (this.centro.getX(),this.centro.getY(),this.raio,this.cor);
+        }
+        catch (Exception erro)
+        {}
+
+        return c;
+    }
+    
+    public Circulo (Circulo modelo)
+    {
+    	this (modelo.getCentro().getX(), modelo.getCentro().getY(), modelo.raio,modelo.cor);
+    }
+    
 }
